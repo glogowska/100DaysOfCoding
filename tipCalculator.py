63f0a0a0a95edcf832f1412e09567fc22bd859c0
Olalala <109037388+glogowska@users.noreply.github.com>
2022-07-11 15:57:56 +0200

    updated tipCalculator.py
    Changing the line that was responsible for rounding the result to the one that includes zeros on decimal places.

--- tipCalculator.py
+++ tipCalculator.py
@@ -4,6 +4,10 @@ total_bill = float(input("What was the total bill? $"))
 tip = int(input("What procentage bill would you like to give? 10, 12 or 15? "))
 people = int(input("How many people to split the bill? "))
 
-divided_bill = round((total_bill*(1+tip/100))/people, 2)
+#divided_bill = round((total_bill*(1+tip/100))/people, 2)
 
-print(f"Each person should pay: ${divided_bill}")
+divided_bill1 = total_bill*(1+tip/100)/people
+
+divided_bill2 ="{:.2f}".format(divided_bill1)
+
+print(f"Each person should pay: ${divided_bill2}")

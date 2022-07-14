9aa95e296a46ed09cdbdd03845673b06d4508c1d
Olalala <109037388+glogowska@users.noreply.github.com>
2022-07-14 23:44:41 +0200

    DAY 5- password generator
    I created a simple password generator, which gives the user a random mix of letters, symbols and numbers in quantities, which were previously chosen by the user.

---
+++ passwordGenerator.py
@@ -0,0 +1,31 @@
+#Password Generator Project
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters= int(input("How many letters would you like in your password?\n")) 
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+characters = []
+
+for l in range(0,nr_letters):
+  a = random.randint(0,len(letters)-1)
+  characters.append(letters[a])
+
+for s in range(0,nr_symbols):
+  b = random.randint(0,len(symbols)-1)
+  characters.append(symbols[b])
+
+for n in range(0,nr_numbers):
+  c = random.randint(0,len(numbers)-1)
+  characters.append(numbers[c])
+  
+password = ""
+random.shuffle(characters)
+for character in characters:
+  character = str(character)
+  password += character
+
+print(password)
